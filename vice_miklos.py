510a0eb702e803792ac00fdbd9ab6065b6b138fb
Luca <mikloska973@gmail.com>
2018-02-05 09:28:36 -0500

    added crude nested if loop file

---
+++ vice_miklos.py
@@ -0,0 +1,63 @@
+import gun
+from sys import exit
+from random import randint
+
+class Scene(object):
+
+	def enter(self):
+		pass
+
+class Engine(object):
+
+	def __init__(self,scene_map):
+		pass
+
+	def play(self):
+		pass
+
+class Death(Scene):
+
+	def enter(self):
+		pass
+
+class Mistake(Scene):
+
+	def enter(self):
+		pass
+
+class Heist(Scene):
+
+	def enter(self):
+		pass
+
+class Chase(Scene):
+
+	def enter(self):
+		pass
+
+class Court(Scene):
+
+	def enter(self):
+		pass
+
+class Jail(Scene):
+
+	def enter(self):
+		pass
+
+class Map(object):
+
+	def __init__(self,start_scene):
+		pass
+
+
+	def next_scene(self, scene_name):
+		pass
+
+
+	def opening_scene(self):
+		pass
+
+a_map= Map('heist')
+a_game=Engine(a_map)
+a_game.play()
